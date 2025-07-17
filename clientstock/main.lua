68fd1512e18ac02470daf682f2216e7a3aff4dc8
Artisine <artisinedev@gmail.com>
2025-07-17 22:50:27 +0100

    Basic clientstock framework

--- clientstock/main.lua
+++ clientstock/main.lua
@@ -1,12 +1,60 @@
 
-
+local Logger = require("utils.simpleLogging")
+local log = Logger.new("/Docs/clientstock.log")
 local cryptoNet = require("utils.cryptoNet")
-cryptoNet.
+
+-- DEFAULT_PRINT = print
+-- print = function(...)
+-- 	local output = table.concat({...}, " ")
+-- 	log:info(output)
+-- 	DEFAULT_PRINT(...)
+-- end
+
+log:info("Starting the server...")
+log:warn("awooga")
 
 
+local thisUserSocket = nil
+local function onStart()
+	local socket = cryptoNet.connect("Cinnamon-AE2-ME-Requester")
+	if not socket then
+		log:error("Failed to connect to the server.")
+		error("Failed to connect to the server.")
+		return
+	end
+	print("Username: ")
+	local username = read()
+	print("Password: ")
+	local password = read("*")
+	cryptoNet.login(socket, username, password)
+	thisUserSocket = socket
+end
 
+local function onEvent(event)
+	if event[1] == "login" then
+		local username = event[2]
+		local socket = event[3]
+		log:info("User logged in as " .. username .. " successfully.")
+		cryptoNet.send(socket, "Hello from clientstock!")
 
+	elseif event[1] == "login_failed" then
+		local reason = event[2]
+		log:error("Login failed: " .. reason)
+		print("Login failed: " .. reason)
+	elseif event[1] == "connection_closed" then
+		local reason = event[2]
+		log:warn("Connection closed: " .. reason)
+		print("Connection closed: " .. reason)
+	elseif event[1] == "encrypted_message" then
+		local message = event[2]
+		local socket = event[3]
+		local server = event[4]
 
+		print(message)
+	end
+end
+
+cryptoNet.startEventLoop(onStart, onEvent)
 
 
 
